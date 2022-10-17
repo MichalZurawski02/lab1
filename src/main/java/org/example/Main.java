f01c02ebbebd393ea04ae62c6a2b682ef3816b83
Nina Mądra <99903385+ninamadra@users.noreply.github.com>
2022-10-17 12:22:22 +0200

    Update Main.java
    konflikt -_-

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
+        int x = 7919;
         System.out.println("Hello world!");
     }
 }
